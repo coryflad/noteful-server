3b4fd447a09d61118d219301af11d4214405b1b3
Cory Fladhammer <cjfladham@hotmail.com>
2020-08-22 20:49:03 -0500

    add connection to live database

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/noteful',
+  DB_URL: process.env.DB_URL || 'postgres://resvfamigkzmxt:4da6ab812aaadff92622cb3f45c09a3d05a7102d70a4f670eeda63d3fa4b0fa9@ec2-107-20-104-234.compute-1.amazonaws.com:5432/d96psbq8l1vmm4',
   API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
 }
